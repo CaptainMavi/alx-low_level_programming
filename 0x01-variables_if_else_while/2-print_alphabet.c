9b94df6d17f304fbb1a2066615f93d13d2b68982
CaptainMavi <silindila@gmail.com>
2023-07-14 19:00:31 -0700

    ccommit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+/**
+ * main-Prints Alphabe
+ *
+ * Return Always 0(Success)
+ */
+int main (void)
+{ 
+        char alp[26]="abcdefghijklmnopqrstuvwxyz",
+	     int i;
+for (i=0;i<26;i++)
+{putchar(alph[i];
+		}
+		putchar('\n');
+		return(0)
+		}
